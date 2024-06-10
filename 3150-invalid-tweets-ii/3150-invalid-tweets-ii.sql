09afacbb722053465466501aa9dc652d9430cdc8
Venu Madhav Pendurthi <92699159+V1125333@users.noreply.github.com>
2024-06-10 09:37:08 -0500

    Time: 296 ms (93.96%), Space: 0B (100.00%) - LeetHub

---
+++ 3150-invalid-tweets-ii/3150-invalid-tweets-ii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+
+SELECT tweet_id
+FROM Tweets
+WHERE
+    CHAR_LENGTH(content) > 140 
+    OR
+    (CHAR_LENGTH(content) - CHAR_LENGTH(REPLACE(content, '@', '')))> 3 
+    OR
+    (CHAR_LENGTH(content) - CHAR_LENGTH(REPLACE(content, '#', '')))> 3
+ORDER BY 
+    tweet_id ASC;
+
+
+
